19378a063b741e552ea3584016c6dea153391e92
Abhishek Jain <abhi_632@rediffmail.com>
2016-08-08 01:15:01 -0700

    Updated EC2.py

--- EC2.py
+++ EC2.py
@@ -2,8 +2,16 @@ import time
 from boto.ec2 import connect_to_region
 ec2_conn=connect_to_region('us-west-2',aws_access_key_id='XXXXXXXXXXXXXXXX',aws_secret_access_key='XXXXXXXXXXXXXXXX')
 
-demo_sg=ec2_conn.create_security_group("demo_sg","Demo security group")
-demo_sg.authorize("tcp",22,22,"0.0.0.0/0")
+f_sg_present=-1
+for sg in ec2_conn.get_all_security_groups():
+	if sg.name=="demo_sg":
+		demo_sg=sg
+		f_sg_present=0
+		break
+
+if f_sg_present==-1:
+	demo_sg=ec2_conn.create_security_group("demo_sg","Demo security group")
+	demo_sg.authorize("tcp",22,22,"0.0.0.0/0")
 
 demo_key=ec2_conn.create_key_pair("demo_key")
 demo_key.save('.')
