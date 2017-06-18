c5ca56b53afa68ac88c88ed25b17404777646acc
Abhishek Jain <abhi_632@rediffmail.com>
2017-06-18 20:26:27 +0530

    Update EC2_Instance.py

--- EC2_Instance.py
+++ EC2_Instance.py
@@ -1,4 +1,5 @@
 #new comment
+#new comment 2
 import time
 from boto.ec2 import connect_to_region
 ec2_conn=connect_to_region('us-west-2',aws_access_key_id='XXXXXXXXXXXXXXXX',aws_secret_access_key='XXXXXXXXXXXXXXXX')
